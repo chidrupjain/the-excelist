fec59d49be61ba9f045701ae58496d548a36e8ab
chidrupjain <chidroopjain@gmail.com>
2025-12-12 15:01:19 +0530

    feat: Convert application to static HTML and ES Modules
    The application has been refactored to utilize a static HTML structure rendered directly in `index.html`, eliminating the need for React DOM and simplifying the build process. This change allows for direct ES Module imports via CDN.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used as the application has been converted to a static HTML structure in index.html.
+// To revert to React, restore the App component mounting logic here.
